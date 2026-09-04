12e7fbbb1d382561b6f81b281a6854020882b7be
Wei <91066116+weimi95@users.noreply.github.com>
2026-09-04 17:50:49 +0800

    docs/brand: 应用内与应用名统一为微图相册管家，关于页致谢 Aves 原项目

--- lib/widgets/about/app_ref.dart
+++ lib/widgets/about/app_ref.dart
@@ -8,7 +8,7 @@ import 'package:aves/widgets/common/identity/aves_logo.dart';
 import 'package:flutter/material.dart';
 
 class AppReference extends StatelessWidget {
-  static const avesGithub = 'https://github.com/deckerst/aves';
+  static const avesGithub = 'https://github.com/weimi95/weitu';
   static const avesFaq = '$avesGithub/wiki/FAQ';
 
   const AppReference({super.key});
@@ -25,6 +25,9 @@ class AppReference extends StatelessWidget {
             spacing: 16,
             crossAxisAlignment: .center,
             children: AppReference.buildLinks(context),
+      const SizedBox(height: 8),
+      const Text('基于 Aves 二次开发，感谢原作者 deckerst 及所有贡献者',
+          textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: Colors.grey)),
           ),
         ],
       ),
