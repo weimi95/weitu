eab163ebfd3ed7c26eefa0bb065059ce6ccb877e
Wei <91066116+weimi95@users.noreply.github.com>
2026-09-12 17:39:28 +0800

    fix(moments): import entry props extension so isVideo resolves

--- lib/widgets/moments/moments_card.dart
+++ lib/widgets/moments/moments_card.dart
@@ -1,4 +1,5 @@
 import 'package:aves/model/entry/entry.dart';
+import 'package:aves/model/entry/extensions/props.dart';
 import 'package:aves/model/settings/settings.dart';
 import 'package:aves/model/source/collection_lens.dart';
 import 'package:aves/model/source/collection_source.dart';
